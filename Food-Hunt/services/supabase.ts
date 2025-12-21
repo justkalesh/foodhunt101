96b4710639be7f555126f92ffd00578131a27687
justkalesh <parth.ie.kalash@gmail.com>
2025-12-21 16:30:32 +0530

    Bug fix

--- Food-Hunt/services/supabase.ts
+++
@@ -1,8 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-// TODO: Move these to environment variables (.env)
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
